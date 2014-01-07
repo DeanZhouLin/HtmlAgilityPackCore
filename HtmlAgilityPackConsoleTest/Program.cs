4e360f9c625eff3f0f45d88f10c8cbd30d02dc6b
DeanZhouLin <funny_zhoulin@163.com>
2014-01-07 15:05:49 +0800

    Add Solution

---
+++ HtmlAgilityPackConsoleTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPackHelperLib;
+
+namespace HtmlAgilityPackConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var htmlDocument = HtmlAgilityPackHelper.CreateHtmlDocument();
+            var html = "<a src=\"http://www.1juhao.com/descpics/images/1102880001/1.jpg\"></a><iframe src=\"http://www.1juhao.com/descpics/images/1102940001/2.jpg\"><p rowspan=\"3\" colspan=\"2\" >Your browser does not support iframes.</p></iframe>";
+            htmlDocument.LoadHtml(html);
+
+            var res = htmlDocument.GetEntireNodes();
+
+            res.Where(c=>c.na)
+            var nodes = htmlDocument.DocumentNode.SelectNodes("*");
+            foreach (var node in nodes)
+            {
+                foreach (var attr in node.Attributes)
+                {
+                    
+                    Console.WriteLine(node.Name + "|" + attr.Name + "|" + attr.Value);
+                }
+
+                node.Attributes.Add(node.Attributes[0].Name + "1", node.Attributes[0].Value);
+
+            }
+
+            Console.WriteLine(nodes.Count);
+            Console.WriteLine(htmlDocument.ParseString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HtmlAgilityPackHelperLib/HtmlAgilityPackHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HtmlAgilityPackHelperLib
+{
+    public static class HtmlAgilityPackHelper
+    {
+        public static HtmlDocument CreateHtmlDocument()
+        {
+            return new HtmlDocument();
+        }
+
+        public static string ParseString(this HtmlDocument doc)
+        {
+            var sb = new StringBuilder();
+            using (var writer = new StringWriter(sb))
+            {
+                doc.Save(writer);
+            }
+            return sb.ToString();
+        }
+
+        public static List<HtmlNode> GetEntireNodes(this HtmlDocument doc)
+        {
+            var nodes = doc.DocumentNode.SelectNodes("*");
+            Stack<HtmlNode> execStack = new Stack<HtmlNode>();      
+            List<HtmlNode> res = new List<HtmlNode>();
+            foreach (var node in nodes)
+            {
+                execStack.Push(node);
+                while (execStack.Count > 0)
+                {
+                    var node1 = execStack.Pop();
+                    res.Add(node1);
+                    var tempNodes = node1.ChildNodes;
+                    if (tempNodes != null && tempNodes.Count > 0)
+                    {
+                        foreach (var tempNode in tempNodes)
+                        {
+                            execStack.Push(tempNode);
+                        }
+                    }
+                }
+            }
+            
+           
+            return res;
+        }
+    }
+
+
+}
